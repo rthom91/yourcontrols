449a7d42c7855cbb7d5f30d07905f5ffa48a7d82
Randy T <47700786+rthom91@users.noreply.github.com>
2026-02-14 18:30:49 -0500

    Module changes
    Add syntax
    Corrected mouseup
    Improved hashing
    Less ID collisions
    Fixed loop clearing
    Decreased polling latency
    Removed redundant processinput
    Nuke CJ4 toggle box
    Cleanup
    Bump version

--- scripts/build-include/community/YourControls/html_ui/YourControls/Main.js
+++ scripts/build-include/community/YourControls/html_ui/YourControls/Main.js
@@ -1,30 +1,38 @@
-var handler = null
+var handler = null;
 
 class YourControlsHandler {
 	constructor(instrumentName, isTouch) {
-		this.isTouch = isTouch
-		this.instrumentName = instrumentName
+		this.isTouch = isTouch;
+		this.instrumentName = instrumentName;
 
-		this.net = new YourControlsNetwork(this.onMessage.bind(this), this.onConnected.bind(this), this.onDisconnected.bind(this), () => this.canProcess() || this.isTouch)
-		this.events = new YourControlsHTMLEvents(this.onButton.bind(this), this.onInput.bind(this))
+		this.net = new YourControlsNetwork(
+			this.onMessage.bind(this),
+			this.onConnected.bind(this),
+			this.onDisconnected.bind(this),
+			() => this.canProcess() || this.isTouch
+		);
+		this.events = new YourControlsHTMLEvents(
+			this.onButton.bind(this),
+			this.onInput.bind(this)
+		);
 
-		this.panelId = Math.floor(Math.random() * 100000)
-		// Only one panel should fire event
-		SimVar.SetSimVarValue("L:YourControlsPanelId", "Number", this.panelId)
+		this.panelId = Math.floor(Math.random() * 100000);
 
-		this.start()
+		SimVar.SetSimVarValue("L:YourControlsPanelId", "number", this.panelId); // Only one panel should fire event
+
+		this.start();
 	}
 
 	canProcess() {
-		return SimVar.GetSimVarValue("L:YourControlsPanelId", "Number") == this.panelId
+		return SimVar.GetSimVarValue("L:YourControlsPanelId", "number") == this.panelId;
 	}
 
 	start() {
-		setTimeout(this.startCall.bind(this), 3000)
+		setTimeout(this.startCall.bind(this), 3000);
 	}
 
 	startCall() {
-		this.net.startAttemptConnection(this.instrumentName)
+		this.net.startAttemptConnection(this.instrumentName);
 	}
 
 	onMessage(data) {
@@ -62,40 +70,36 @@ class YourControlsHandler {
 	}
 
 	onConnected() {
+		if (this.instrumentName.includes("WT21_FMC")) return;
 		if (this.isTouch) {
-			this.events.bindEvents()
-			this.events.startDocumentListener()
+			this.events.bindEvents();
+			this.events.startDocumentListener();
 		}
 	}
 
 	onDisconnected() {
-		this.events.clear()
+		this.events.clear();
 	}
 
 	onInput(elementId, value) {
-		this.net.sendInputEvent(elementId, value)
+		this.net.sendInputEvent(elementId, value);
 	}
 
 	onButton(elementId) {
-		this.net.sendInteractionEvent("H:YCB" + this.instrumentName + "#" + elementId)
+		this.net.sendInteractionEvent("H:YCB" + this.instrumentName + "#" + elementId);
 	}
 
 	processInteractionEvent(args) {
-		// Panel event
 		if (args[0].startsWith("YCB")) {
-
-			YourControlsHTMLTrigger.setPanel(args[0].substring(3), this.instrumentName)
-			return false
-
+			YourControlsHTMLTrigger.setPanel(args[0].substring(3), this.instrumentName);
+			return false;
 		} else if (args[0].startsWith("YCH")) {
-
-			args[0] = args[0].substring(3)
-
+			args[0] = args[0].substring(3);
 		} else {
-			if (this.canProcess()) { // Only one gauge should send interaction button events
-				this.net.sendInteractionEvent("H:YCH" + args[0])
+			if (this.canProcess()) { // Only one panel should send interaction button events
+				this.net.sendInteractionEvent("H:YCH" + args[0]);
 			}
 		}
-		return true
+		return true;
 	}
 }
